c896b8d2918ad2411434144b2a6fba9f83c119a2
Paulo Vitor <paulo.vitor@alunos.ifsuldeminas.edu.br>
2023-05-24 10:37:25 -0300

    Aula 17

--- aula12/rodape.php
+++ aula12/rodape.php
@@ -1,7 +1,7 @@
 <hr>
 
 <!-- Rodapé -->
-<footer class="bg-dark h-4">
+<footer class="bg-dark p-3 text-white text-center">
     Meu site de notícias @ 2023
 </footer>
 
--- aula13/noticia/formulario.php
+++ aula13/noticia/formulario.php
@@ -1,17 +1,9 @@
 <?php
     require_once "../banco/conexao.php";
     require_once "consultar_por_id.php";
+    require_once "../template/cabecalho.php";
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<div class="container">
     <h1>Cadastro de notícias</h1>
     <hr>
 
@@ -20,23 +12,21 @@
         
         <input type="hidden" name="idnoticia" value="<?php echo $noticia->idnoticia ?? "";?>"><br>
 
-        <label>Título</label><br>
-        <input type="text" name="titulo" value="<?php echo $noticia->titulo ?? "";?>"><br>
+        <label class="form-label">Título</label><br>
+        <input class="form-control" type="text" name="titulo" value="<?php echo $noticia->titulo ?? "";?>"><br>
 
-        <label>Matéria</label><br>
-        <input type="text" name="materia" value="<?php echo $noticia->materia ?? "";?>"><br>
+        <label class="form-label">Matéria</label><br>
+        <textarea class="form-control" type="text" name="materia"><?php echo $noticia->materia ?? "";?></textarea><br>
 
-        <label>Categoria</label><br>
-        <input type="text" name="categoria" value="<?php echo $noticia->categoria ?? "";?>"><br>
+        <label class="form-label">Categoria</label><br>
+        <input class="form-control" type="text" name="categoria" value="<?php echo $noticia->categoria ?? "";?>"><br>
 
-        <label>Foto</label><br>
-        <input type="file" name="foto"><br>
+        <label class="form-label">Foto</label><br>
+        <input class="form-control" type="file" name="foto"><br>
 
-        <button type="submit">Inserir</button>
+        <button type="submit" class="btn btn-success">Inserir</button>
         
     </form>
+</div>
 
-
-    
-</body>
-</html>
+<?php require_once "../template/rodape.php"; ?>
