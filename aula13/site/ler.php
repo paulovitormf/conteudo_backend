2268c964442a73c4a6d56406d35bd8734ede2bcf
Paulo Vitor <paulo.vitor@alunos.ifsuldeminas.edu.br>
2023-05-30 08:38:07 -0300

    Aula 18

---
+++ aula13/site/ler.php
@@ -0,0 +1,15 @@
+<?php
+include_once "../noticia/consultar_por_id.php";
+include_once "../template/cabecalho.php";
+include_once "../template/menu.php";
+
+?>
+
+<div class="container">
+    <h1><?php echo $noticia->titulo; ?></h1>
+    <hr>
+    <img src="../uploads/<?php echo $noticia->foto; ?>" class="">
+    <p><?php echo $noticia->materia; ?></p>
+</div>
+
+<?php include_once "../template/rodape.php"; ?>
