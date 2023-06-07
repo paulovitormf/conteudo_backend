24160fcece86376bd827c9b4d2b4622cb72b758c
Paulo Vitor <paulo.vitor@alunos.ifsuldeminas.edu.br>
2023-06-07 10:25:29 -0300

    Aula 20

--- aula13/noticia/consultar_todos.php
+++ aula13/noticia/consultar_todos.php
@@ -4,17 +4,25 @@
    require_once "../banco/conexao.php";
 
    //cria uma variável com um comando SQL
-   $SQL = "SELECT * FROM `noticia`";
+   $SQL = "SELECT * FROM `noticia` where `categoria` like ?";
  
    //prepara o comando para ser executado no mysql
    $comando = $conexao->prepare($SQL);
+
+   //pega o valor assunto enviado via GET pela URL
+   //ou um valor em branco
+   $assunto = $_GET['assunto'] ?? "";
+   $assunto = "%$assunto%";
+
+   //vincula a variável $assunto com o param ? no SQL
+   $comando->bind_param("s", $assunto);
 
    //executa o comando
    $comando->execute();
 
    //pegar os resultados da consulta - todas as linhas de resultados
    $resultados = $comando->get_result();
-   
+
    //pega a primeira linha de resultado da consulta
    $noticias = [];
    while($noticia = $resultados->fetch_object()){
