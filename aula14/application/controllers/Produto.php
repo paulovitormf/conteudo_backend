b9bc1685c4abef786bcd74e86caaedb89098fd98
Paulo Vitor <paulo.vitor@alunos.ifsuldeminas.edu.br>
2023-06-13 08:41:42 -0300

    Aula14

---
+++ aula14/application/controllers/Produto.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Produto extends CI_Controller {
+	public function index()
+	{
+        $crud = new Grocery_CRUD();
+        $crud->set_table("produto");
+        $crud->set_field_upload("foto");
+        $form = $crud->render();
+		$this->load->view('crud', $form);
+	}
+	public function noticia()
+	{
+        $crud = new Grocery_CRUD();
+        $crud->set_table("noticia");
+        //$crud->set_field_upload("foto");
+        $form = $crud->render();
+		$this->load->view('crud', $form);
+	}
+}
